0d640ea2eec0647c5d2a81f9bf3cf492798ba68d
Nobuki Yoda <nobuki.y@gmail.com>
2014-07-27 18:11:40 +0900

    Defined valid program format.

--- ml2gcc/ml2gcc.ml
+++ ml2gcc/ml2gcc.ml
@@ -4,5 +4,12 @@ open Gcc
 
 let _ =
   let expr = (Parser.expr Lexer.token (Lexing.from_channel stdin)) in
-  let (main, other) = compile expr [] in
-  print_gccs (main @ [GRtn] @ other)
+  match expr with
+	| ELetIn ([("main", body)], EConst 0) ->
+	  let (args, body) =
+		(match body with
+		  | EFun (args, funbody) -> (args, funbody)
+		  | _ -> ([], body)) in
+	  let (gcc_body, gcc_funs) = compile body args in
+	  print_gccs (gcc_body @ [GRtn] @ gcc_funs)
+	|_ -> raise (Compile_error "Program must be of the form: let main ... = ... in 0.")
