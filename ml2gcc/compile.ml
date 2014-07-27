b3c1373b8c31a64a50fad449999e3db3c169842e
Nobuki Yoda <nobuki.y@gmail.com>
2014-07-27 17:20:58 +0900

    Impremented recursive call.

--- ml2gcc/compile.ml
+++ ml2gcc/compile.ml
@@ -1,6 +1,8 @@
 open Expr
 open Gcc
 
+exception Compile_error of string
+
 let id_num = ref 0
 
 let next_id () = (id_num := !id_num + 1; ("_" ^ (string_of_int !id_num)))
@@ -13,6 +15,12 @@ let rec make_ld env name n i = match env with
 		then GLd (n, i)
 		else make_ld (ss::es) name n (i + 1)
 	  | [] -> make_ld es name (n + 1) 0)
+  | [] -> raise (Compile_error "Variable not found.")
+
+let rec range start last =
+  if start == last then []
+  else if start < last then start::(range (start + 1) last)
+  else start::(range (start - 1) last)
 
 let rec compile2 exp env =
   let cp exp = compile2 exp env in
@@ -42,6 +50,18 @@ let rec compile2 exp env =
 	  let body = (compile2 result (names::env)) in
 	  (closures := (!closures) @ [GLabel label] @ body @ [GRtn];
 	   (List.flatten gccs) @ [GLdf label; (GAp (List.length names))])
+	| ERecIn (lets, result) ->
+	  let let_count = List.length lets in
+	  let names = List.map fst lets in
+	  let label = next_id () in
+	  let body = (compile2 result (names::env)) in
+	  let dummy_env = List.map (fun _ -> GLdc 0) names in
+	  let enter_body = [GLdf label; (GAp let_count)] in
+	  let rec_compile x = compile2 (snd x) (names::env) in
+	  let funs_gcc = List.flatten (List.map rec_compile lets) in
+	  let rewrite_env = List.map (fun i -> GSt (0, i - 1)) (range let_count 0) in
+	  (closures := (!closures) @ [GLabel label] @ funs_gcc @ rewrite_env @ body @ [GRtn];
+	   dummy_env @ enter_body)
 	| EIf (a, t, f) ->
 	  let t_label = next_id () in
 	  let f_label = next_id () in
