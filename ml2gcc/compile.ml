e0fb6147ded8423fab5def0e2b2b5ca24b33496e
Nobuki Yoda <nobuki.y@gmail.com>
2014-07-28 10:20:42 +0900

    Tail call optimization.

--- ml2gcc/compile.ml
+++ ml2gcc/compile.ml
@@ -25,8 +25,8 @@ let rec range start last =
   else if start < last then start::(range (start + 1) last)
   else start::(range (start - 1) last)
 
-let rec compile2 (pos, exp) env =
-  let cp exp = compile2 exp env in
+let rec compile2 (pos, exp) env tail =
+  let cp exp = compile2 exp env false in
   match exp with
 	| EConst value -> [GLdc value]
 	| EAdd (a, b) -> (cp a) @ (cp b) @ [GAdd]
@@ -43,25 +43,26 @@ let rec compile2 (pos, exp) env =
 	| EDebug a -> (cp a) @ [GDbug; GLdc 0]
 	| EFun (names, body) ->
 	  let label = next_id () in
-	  let body = (compile2 body (names::env)) in
+	  let body = (compile2 body (names::env) true) in
 	  (closures := (!closures) @ [GLabel label] @ body @ [GRtn];
 	   [GLdf label])
 	| EVar name -> [make_ld env name pos 0 0]
-	| EApp (a, b) -> (flatten (map cp b)) @ (cp a) @ [GAp (length b)]
+	| EApp (a, b) -> (flatten (map cp b)) @ (cp a)
+	  @ [if tail then GTap (length b) else GAp (length b)]
 	| ELetIn (lets, result) ->
 	  let gccs = map (fun x -> cp (snd x)) lets in
 	  let label = next_id () in
-	  let body = (compile2 result ((map fst lets)::env)) in
+	  let body = (compile2 result ((map fst lets)::env) true) in
 	  (closures := (!closures) @ [GLabel label] @ body @ [GRtn];
-	   (flatten gccs) @ [GLdf label; (GAp (length lets))])
+	   (flatten gccs) @ [GLdf label; if tail then (GTap (length lets)) else (GAp (length lets))])
 	| ERecIn (lets, result) ->
 	  let let_count = length lets in
 	  let new_env = (map fst lets)::env in
 	  let label = next_id () in
-	  let body = (compile2 result new_env) in
+	  let body = (compile2 result new_env true) in
 	  let dummy_env = List.map (fun _ -> GLdc 0) lets in
-	  let enter_body = [GLdf label; (GAp let_count)] in
-	  let rec_compile x = compile2 (snd x) new_env in
+	  let enter_body = [GLdf label; if tail then (GTap let_count) else (GAp let_count)] in
+	  let rec_compile x = compile2 (snd x) new_env false in
 	  let funs_gcc = List.flatten (List.map rec_compile lets) in
 	  let rewrite_env = List.map (fun i -> GSt (0, i - 1)) (range let_count 0) in
 	  (closures := (!closures) @ [GLabel label] @ funs_gcc @ rewrite_env @ body @ [GRtn];
@@ -69,12 +70,14 @@ let rec compile2 (pos, exp) env =
 	| EIf (a, t, f) ->
 	  let t_label = next_id () in
 	  let f_label = next_id () in
+	  let (sel, join) = if tail then (GTsel(t_label, f_label), GRtn)
+		else (GSel(t_label, f_label), GJoin) in
 	  (closures := (!closures)
-	   @ [GLabel t_label] @ (cp t) @ [GJoin]
-	   @ [GLabel f_label] @ (cp f) @ [GJoin];
-	   (cp a) @ [GSel (t_label, f_label)])
+	   @ [GLabel t_label] @ (compile2 t env tail) @ [join]
+	   @ [GLabel f_label] @ (compile2 f env tail) @ [join];
+	   (cp a) @ [sel])
 
 let compile exp toplevel =
   let _ = (closures := []) in
-  let gcc = compile2 exp [toplevel] in
+  let gcc = compile2 exp [toplevel] false in
   (gcc, !closures)
